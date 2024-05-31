df77d4546683ed4dd40e11f5878ccf9ade3efa62
NarcisKG <dusan224@gmail.com>
2024-05-31 17:39:53 +0200

    Commit to Git

--- src/main/java/Core/Locators.java
+++ src/main/java/Core/Locators.java
@@ -4,28 +4,6 @@ import org.openqa.selenium.By;
 
 public class Locators {
 
-    public static String DEMO_QA_SECTION = "//h5[text()='%ARG%']//parent::div";
-
-    public static String SPAN_ELEMENT_BY_TEXT = "//span[text()='%ARG%']";
-
-    public static String ELEMENT_LIST_SHOW = "//div[@class='element-list collapse show']";
-
-    public static String INPUT_BY_ID = "//input[@id='%ARG%']";
-
-    public static String HEADER_SAMPLE_PAGE = "//h1[@id='sampleHeading']";
-
-    public static String ALERT = "//button[@id='alertButton']";
-
-    public static String ALERT_TIMER = "//button[@id='timerAlertButton']";
-
-    public static String CONFIRM_BUTTON = "//button[@id='confirmButton']";
-
-    public static String PROMT_BUTTON = "//button[@id='promtButton']";
-
-    public static String NEW_TAB_BUTTON = "//button[@id='tabButton']";
-
-    public static String NEW_WINDOW_BUTTON = "//button[@id='windowButton']";
-
     public static By brandSearchField = By.xpath("//div[@class='SumoSelect sumo_brand']//input");
     public static By  setBrand (String brandName){
         String brandSearchLocator = "//div[contains(@class, 'open')]//li//label[text()='****']";
---
+++ src/main/java/Core/LocatorsDemoQA.java
@@ -0,0 +1,26 @@
+package Core;
+
+public class LocatorsDemoQA {
+
+    public static String DEMO_QA_SECTION = "//h5[text()='%ARG%']//parent::div";
+
+    public static String SPAN_ELEMENT_BY_TEXT = "//span[text()='%ARG%']";
+
+    public static String ELEMENT_LIST_SHOW = "//div[@class='element-list collapse show']";
+
+    public static String INPUT_BY_ID = "//input[@id='%ARG%']";
+
+    public static String HEADER_SAMPLE_PAGE = "//h1[@id='sampleHeading']";
+
+    public static String ALERT = "//button[@id='alertButton']";
+
+    public static String ALERT_TIMER = "//button[@id='timerAlertButton']";
+
+    public static String CONFIRM_BUTTON = "//button[@id='confirmButton']";
+
+    public static String PROMT_BUTTON = "//button[@id='promtButton']";
+
+    public static String NEW_TAB_BUTTON = "//button[@id='tabButton']";
+
+    public static String NEW_WINDOW_BUTTON = "//button[@id='windowButton']";
+}
